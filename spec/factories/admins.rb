cdb83a3a0e08d9b498911848ab5afc1a9683282d
killer-cf <costa.kilder@live.com>
2022-08-20 11:42:49 -0300

    admin sings in

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,8 @@
+require 'faker'
+
 FactoryBot.define do
   factory :admin do
-    
+    email { Faker::Internet.email }
+    password { 'password' }
   end
 end
---
+++ spec/system/authentication/admin_sings_in_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe 'admin sings in' do
+  it 'success' do
+    create :admin, email: 'kilder@example.com'
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: 'kilder@example.com'
+    fill_in 'Senha', with: 'password'
+    find('.actions').click_on 'Entrar'
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content('kilder@example.com')
+    expect(page).not_to have_link('Entrar')
+    expect(page).to have_content('Autenticação efetuada com sucesso.')
+  end
+
+  it 'with blank fields' do
+    create :admin, email: 'kilder@example.com'
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'E-mail', with: ''
+    fill_in 'Senha', with: ''
+    find('.actions').click_on 'Entrar'
+
+    expect(page).to have_content 'E-mail ou senha inválidos.'
+    expect(page).not_to have_content 'Autenticação efetuada com sucesso.'
+  end
+
+  it 'with incorrect fields' do
+    create :admin, email: 'kilder@example.com'
+
+    visit new_admin_session_path
+    fill_in 'E-mail', with: 'kilderexample.com'
+    fill_in 'Senha', with: '123'
+    find('.actions').click_on 'Entrar'
+
+    expect(page).not_to have_content 'Autenticação efetuada com sucesso.'
+    expect(page).to have_content 'E-mail ou senha inválidos.'
+  end
+end
