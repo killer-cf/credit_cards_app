bae7407887e8bdcaf2715ecc04c043369e0ca134
killer-cf <costa.kilder@live.com>
2022-08-25 11:52:04 -0300

    create card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,23 @@
 class Card < ApplicationRecord
+  validates :name, :cpf, :number, :code, :valid_date, :password, presence: true
+
+  before_validation :generate_password, :generate_number, :generate_code, :generate_valid_date, on: :create
+
+  private
+
+  def generate_password
+    self.password = SecureRandom.random_number(10**4).to_s.rjust(4, '0')
+  end
+
+  def generate_number
+    self.number = SecureRandom.random_number(10**16).to_s.rjust(16, '0')
+  end
+
+  def generate_code
+    self.code = SecureRandom.random_number(10**3).to_s.rjust(3, '0')
+  end
+
+  def generate_valid_date
+    self.valid_date = 5.years.from_now.to_date
+  end
 end
---
+++ spec/system/cards/user_generate_card_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'admin generate new card' do
+  it 'success' do
+    admin = create :admin
+
+    login_as admin, scope: :admin
+    visit root_path
+    click_on 'Gerador de cartão'
+    fill_in 'Nome', with: 'Kilder Costa filho'
+    fill_in 'CPF', with: '72318126434'
+    click_on 'Gerar'
+
+    expect(page).to have_content 'Cartão gerado com sucesso'
+    expect(Card.count).to eq 1
+    card = Card.last
+    expect(card.name.nil?).to eq false
+    expect(card.cpf.nil?).to eq false
+    expect(card.number.nil?).to eq false
+    expect(card.password.nil?).to eq false
+    expect(card.code.nil?).to eq false
+    expect(card.valid_date.nil?).to eq false
+  end
+
+  it 'with blank fields' do
+    admin = create :admin
+
+    login_as admin, scope: :admin
+    visit root_path
+    click_on 'Gerador de cartão'
+    fill_in 'Nome', with: ''
+    fill_in 'CPF', with: ''
+    click_on 'Gerar'
+
+    expect(page).to have_content 'Nome não pode ficar em branco'
+    expect(page).to have_content 'CPF não pode ficar em branco'
+  end
+end
